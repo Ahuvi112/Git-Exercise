0d1fa91634056ee0c4732b146c9099d9a1db00be
dassydayan <dassydayan@gmail.com>
2023-06-21 00:58:40 +0300

    controllerAndValidEmail

--- Models/User.model.js
+++ Models/User.model.js
@@ -20,8 +20,8 @@ const user = {
 
 let users = [];
 
-function get() {
-    return userMoodel;
+function Get() {
+    return users;
 }
 
 function Add(user) {
@@ -37,5 +37,12 @@ function Update(id, user) {
 function Delete(id) {
     users = users.filter(user => user.id != id)
 }
+
+function ValidEmail(email){
+    //need to implement includes
+    if(!email.includes('@') || email.includes(' '))
+        return false;
+    return true;
+} 
 
 export default userMoodel;
