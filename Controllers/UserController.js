7b1417a34ffbbfe73d9e3cc7fa41fd192ac5422e
dassydayan <dassydayan@gmail.com>
2023-06-21 01:30:20 +0300

    controller

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -1,23 +1,33 @@
+// import userMoodel from "../Models/User.model.js";
+
 const UsersController = {
 
     getUsers:  (req, res) => {
-       
+        let users=userMoodel.Get();
+        res.json(users);
     },
 
     getById: (req,res)=>{
-      
+        try{
+            const { id } = req.body;
+            let user =  UserModel.getById(id);
+            res.json(user);
+        }
+        catch(e){
+            res.status(404).json({message:e.message});
+        }
     },
 
     addUser:  (req, res) => {
-       
+    
     },
 
     deleteUser: async (req, res) => {
-       
+        
     },
 
     updateUser: async (req, res) => {
-      
+       
     }
 }
 
