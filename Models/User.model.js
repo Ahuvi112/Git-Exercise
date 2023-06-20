be2a6c58d2709fc37213c870366b2d5562f0d675
Ahuvi112 <ahuvigit@gmail.com>
2023-06-20 21:59:45 +0300

    6/20/23 user_model

---
+++ Models/User.model.js
@@ -0,0 +1,41 @@
+
+const user = {
+    id: {
+        type: Number,
+        require: true
+    },
+    name: {
+        type: String,
+        require: true
+    },
+    email: {
+        type: String,
+        require: true
+    },
+    phone: {
+        type: String,
+        require: true
+    }
+}
+
+let users = [];
+
+function get() {
+    return userMoodel;
+}
+
+function Add(user) {
+    const newUser = user;
+    users.push(newUser);
+}
+
+function Update(id, user) {
+    const index = users.findIndex(user => user.id == id);
+    users[index] = user;
+}
+
+function Delete(id) {
+    users = users.filter(user => user.id != id)
+}
+
+export default userMoodel;
