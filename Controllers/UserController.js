202054533538e6f5b24bb69d67b2db839cfed732
dassydayan <dassydayan@gmail.com>
2023-06-21 13:40:14 +0300

    imports

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -1,14 +1,13 @@
-import {getUserById,get,update,deleteUser,User} from "../Models/User.model.js";
-
+import {Get} from '../Models/User.model.js'
 const UsersController = {
 
     getUsers:  (req, res) => {
-        let users=userMoodel.Get();
+        let users=Get();
         res.json(users);
     },
 
-    getById: (req,res)=>{
-        try{
+    getById: (req, res) => {
+        try {
             const { id } = req.body;
             let user =  UserModel.getById(id);
             res.json(user);
@@ -19,16 +18,35 @@ const UsersController = {
     },
 
     addUser:  (req, res) => {
-    
+        const { name, email, phone } = req.body;
+        try {
+            const newUser = UserModel.Add({ name, email, phone });
+            res.json(newUser);
+        }
+        catch (e) {
+            res.status(400).json({ message: e.message });
+        }
     },
 
     deleteUser: async (req, res) => {
-        
+        try {
+            const id = req.body;
+            UserModel.delete(id);
+        }
+        catch (e) {
+            res.status(404).json({ message: e.message });
+        }
     },
 
-    updateUser: async (req, res) => {
-       
+    updateUser: async (req, res) => { try {
+        const { id, user } = req.body;
+        UserModel.Update(id, user);
+    }
+    catch (e) {
+        res.status(404).json({ message: e.message });
+    }
     }
 }
+       
 
 export default UsersController;
