99aaa4853d96a2491aedb72cb7e5a4695c8ab935
Ahuvi112 <ahuvigit@gmail.com>
2023-06-21 09:47:11 +0300

    controller

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -1,45 +1,21 @@
-<<<<<<< HEAD
-// import userMoodel from "../Models/User.model.js";
 
-const UsersController = {
-
-    getUsers:  (req, res) => {
-        let users=userMoodel.Get();
-        res.json(users);
-    },
-
-    getById: (req,res)=>{
-        try{
-            const { id } = req.body;
-            let user =  UserModel.getById(id);
-            res.json(user);
-        }
-        catch(e){
-            res.status(404).json({message:e.message});
-        }
-    },
-
-    addUser:  (req, res) => {
-    
-    },
-
-    deleteUser: async (req, res) => {
-        
-    },
-
-    updateUser: async (req, res) => {
-       
-=======
-import UserModel from "../Models/Users.model.js";
 
 const UsersController = {
 
     getUsers: (req, res) => {
-
+        let users = userMoodel.Get();
+        res.json(users);
     },
 
     getById: (req, res) => {
-
+        try {
+            const { id } = req.body;
+            let user = UserModel.getById(id);
+            res.json(user);
+        }
+        catch (e) {
+            res.status(404).json({ message: e.message });
+        }
     },
 
     addUser: (req, res) => {
@@ -72,7 +48,6 @@ const UsersController = {
         catch (e) {
             res.status(404).json({ message: e.message });
         }
->>>>>>> 5234cf562a3be94b4bc3d26cddf3d4434353e21e
     }
 }
 
